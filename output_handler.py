6bd524cab74a53c529ad44366976162f4c6be146
Srikar Gouru <srikarg89@gmail.com>
2020-05-28 18:52:21 -0400

    Output handler

--- output_handler.py
+++ output_handler.py
@@ -1,6 +1,25 @@
+# Handler for giving output to drivers
 class OutputHandler:
 
     def __init__(self, config):
         self.config = config
-    
-    
+        self.data = {}
+
+
+    def update_data(self):
+        for subprocess in self.config:
+            self.read_file(subprocess)
+
+
+    def read_file(self, subprocess):
+        filepath = self.config[subprocess]["filepath"]
+        # Read data in file
+        file = open(filepath, "r")
+        data = file.read()
+        file.close()
+        # Clear any data that's in there
+        file = open(filepath, "w")
+        file.write("")
+        file.close()
+        self.data[subprocess] = data
+
